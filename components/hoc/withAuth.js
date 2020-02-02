02dbd742155df3ef30d6dacb0ade437b284b5196
LM Train <layconmm@msn.com>
2020-02-01 23:02:20 -0600

    Roles in Auth0

--- components/hoc/withAuth.js
+++ components/hoc/withAuth.js
@@ -2,37 +2,51 @@ import React from 'react';
 import BaseLayout from '../layout/BaseLayout';
 import BasePage from '../BasePage';
 
+const namespace = 'http://localhost:300/';
 
-
-export default function(Component) {
-    return class withAuth extends React.Component {
+export default role => Component =>
+    class withAuth extends React.Component {
         
-        static async getInitialProps(args) {
-            const pageProps = await Component.getInitialProps && await Component.getInitialProps(args);
+    static async getInitialProps(args) {
+        const pageProps = await Component.getInitialProps && await Component.getInitialProps(args);
 
-            return { ...pageProps };
-            
+        return { ...pageProps };
+        
+    }
+
+    renderProtectedPage() {
+        const { isAuthenticated } = this.props.auth;
+        const userRole = user && user[`${namespace}role`];
+
+        if (role) {
+            if (userRole && userRole === role) { isAuthorized = true};
+        } else {
+            isAuthorized = true;
         }
 
-        renderProtectedPage() {
-            const { isAuthenticated } = this.props.auth;
-    
-            if (isAuthenticated) {
-                return ( <Component {...this.props} />)                
-            } else  {
-                return (                
-                    <BaseLayout {...this.props.auth}>
-                        <BasePage>                                  
-                            <h1>You are not Authenticated. Please login to access Page.</h1>             
-                        </BasePage>
-                    </BaseLayout>
-                )
-            }
+        if (!isAuthenticated) {
+            return (                
+                <BaseLayout {...this.props.auth}>
+                    <BasePage>                                  
+                        <h1>You are not Authenticated. Please login to access Page.</h1>             
+                    </BasePage>
+                </BaseLayout>
+            )
+        } else if (!isAuthorized) {
+            return (
+                <BaseLayout {...this.props.auth}>
+                    <BasePage>                                  
+                        <h1>You are not authorized.you don't have a permission to visit this page.</h1>             
+                    </BasePage>
+                </BaseLayout>
+            )
+        } else {
+            return ( <Component {...this.props} />)
         }
+    }
 
         render() {
             return this.renderProtectedPage()
 
         }
     }
-}
---
+++ pages/owner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import BaseLayout from '../components/layout/BaseLayout';
+import BasePage from '../components/BasePage';
+
+import withAuth from '../components/hoc/withAuth';
+
+class Owner extends React.Component {
+
+    render() {
+        return (
+            <BaseLayout {...this.props.auth}>
+                <BasePage>                                  
+                    <h1>I am the Owner</h1>             
+                </BasePage>
+            </BaseLayout>
+        )
+    }
+}
+
+export default withAuth(Owner);
