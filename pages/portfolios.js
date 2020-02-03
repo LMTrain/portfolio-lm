82e7575d1fbbf9d0f944674f9441d0e558f0955d
LM Train <layconmm@msn.com>
2020-02-02 20:19:07 -0600

    Add Portfolio Card Styling

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import BaseLayout from '../components/layout/BaseLayout';
 import BasePage from '../components/BasePage';
-import Link from 'next/link';
+import { Link } from '../routes'
+// import Link from 'next/link';
+import { Col, Row, Card, CardHeader, CardBody, CardText, CardTitle} from 'reactstrap';
 
 import axios from 'axios';
 
@@ -20,14 +22,24 @@ class Portfolios extends React.Component {
     }    
 
     renderPosts(posts) {        
-        return posts.map((post) => {          
+        return posts.map((post, index) => {          
             return (
-                <li key={post.id}>
-                    {/* <Link as={`/portfolio/${post.id}`} href={`/portfolio?id=${post.id}`}> */}
-                    <Link as={`/portfolio/${post.id}`} href="/portfolio/[id]">
-                        <a style={{'fontSize': '20px'}}>{post.title}</a>
-                    </Link>
-                </li>
+                <Col md="4">
+                    <React.Fragment key={index}>
+                        <span>
+                            <Card className="portfolio-card">
+                                <CardHeader className="portfolio-card-header">Some Position {index}</CardHeader>
+                                <CardBody>
+                                <p className="portfolio-card-city"> Some Location {index} </p>
+                                <CardTitle className="portfolio-card-title">Some Company {index}</CardTitle>
+                                <CardText className="portfolio-card-text">Some Description {index}</CardText>
+                                <div className="readMore"> </div>
+                                </CardBody>
+                            </Card>
+                        </span>
+                    </React.Fragment>
+                </Col>
+
             )
         })
     }
@@ -37,11 +49,11 @@ class Portfolios extends React.Component {
       
         return (            
             <BaseLayout {...this.props.auth}>
-                <BasePage title="Portfolios">                            
-                    <ul>
+                <BasePage className="portfolio-page" title="Portfolios">                            
+                    <Row>
                         { this.renderPosts(posts) }
                     
-                    </ul>
+                    </Row>
                 </BasePage>                
             </BaseLayout>            
         )
