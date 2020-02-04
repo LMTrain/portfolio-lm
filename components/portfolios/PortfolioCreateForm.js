5713d03bb5f7776e771bc79194de62d3232e04f5
LM Train <layconmm@msn.com>
2020-02-03 20:23:30 -0600

    Add more input types

--- components/portfolios/PortfolioCreateForm.js
+++ components/portfolios/PortfolioCreateForm.js
@@ -4,18 +4,24 @@ import React from 'react';
 export default class PortfolioCreateForm extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {title: ''};
+      this.state = {
+        title: '',
+        description: '',
+        language: ''
+      };
   
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
   
     handleChange(event) {
-      this.setState({title: event.target.value});
+      const field = event.target.name;
+      this.setState({[field]: event.target.value});
     }
-  
+
+   
     handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.title);
+      alert('A name was submitted: ' + this.state.title + ' ' + this.state.description + ' ' + this.state.language);
       event.preventDefault();
     }
   
@@ -24,7 +30,20 @@ export default class PortfolioCreateForm extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input name="title" type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <label>
+            Description:
+            <textarea name="description" value={this.state.description} onChange={this.handleChange} />
+          </label>
+          <label>
+            Pick your favorite Programming Language:
+            <select name="language" value={this.state.language} onChange={this.handleChange}>
+              <option value="Javascript">Javascript</option>
+              <option value="Python">Python</option>
+              <option value="Java">Java</option>
+              <option value="C#">C#</option>
+            </select>
           </label>
           <input type="submit" value="Submit" />
         </form>
