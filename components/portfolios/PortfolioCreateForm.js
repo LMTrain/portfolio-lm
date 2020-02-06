8f56bfc1d9aeea12d49821abf0956d2a623a8b3b
LM Train <layconmm@msn.com>
2020-02-05 22:12:31 -0600

    Formik

--- components/portfolios/PortfolioCreateForm.js
+++ components/portfolios/PortfolioCreateForm.js
@@ -1,52 +1,101 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 
-export default class PortfolioCreateForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        title: '',
-        description: '',
-        language: ''
-      };
+const PortfolioCreateForm = () => (
+  <div>    
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 800);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
+export default PortfolioCreateForm;
+
+
+
+
+
+
+
+// import React from 'react';
+
+
+// export default class PortfolioCreateForm extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         title: '',
+//         description: '',
+//         language: ''
+//       };
   
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
+//       this.handleChange = this.handleChange.bind(this);
+//       this.handleSubmit = this.handleSubmit.bind(this);
+//     }
   
-    handleChange(event) {
-      const field = event.target.name;
-      this.setState({[field]: event.target.value});
-    }
+//     handleChange(event) {
+//       const field = event.target.name;
+//       this.setState({[field]: event.target.value});
+//     }
 
    
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.title + ' ' + this.state.description + ' ' + this.state.language);
-      event.preventDefault();
-    }
+//     handleSubmit(event) {
+//       alert('A name was submitted: ' + this.state.title + ' ' + this.state.description + ' ' + this.state.language);
+//       event.preventDefault();
+//     }
   
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input name="title" type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <label>
-            Description:
-            <textarea name="description" value={this.state.description} onChange={this.handleChange} />
-          </label>
-          <label>
-            Pick your favorite Programming Language:
-            <select name="language" value={this.state.language} onChange={this.handleChange}>
-              <option value="Javascript">Javascript</option>
-              <option value="Python">Python</option>
-              <option value="Java">Java</option>
-              <option value="C#">C#</option>
-            </select>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }
-  }
+//     render() {
+//       return (
+//         <form onSubmit={this.handleSubmit}>
+//           <label>
+//             Name:
+//             <input name="title" type="text" value={this.state.value} onChange={this.handleChange} />
+//           </label>
+//           <label>
+//             Description:
+//             <textarea name="description" value={this.state.description} onChange={this.handleChange} />
+//           </label>
+//           <label>
+//             Pick your favorite Programming Language:
+//             <select name="language" value={this.state.language} onChange={this.handleChange}>
+//               <option value="Javascript">Javascript</option>
+//               <option value="Python">Python</option>
+//               <option value="Java">Java</option>
+//               <option value="C#">C#</option>
+//             </select>
+//           </label>
+//           <input type="submit" value="Submit" />
+//         </form>
+//       );
+//     }
+//   }
