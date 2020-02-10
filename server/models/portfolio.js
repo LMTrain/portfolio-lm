80478de0afb9d8b77b7b34e6222b050d6bb71536
LM Train <layconmm@msn.com>
2020-02-10 16:44:04 -0600

    Create Portfolio Model

--- server/models/portfolio.js
+++ server/models/portfolio.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//FUNCTION TO SET STRING TYPES
 const setStringType = (maxLength) => ({ type: String, required: true, maxlength: maxLength })
 
 const portfolioSchema = new Schema({
