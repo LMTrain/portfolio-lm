94bc1e97dac1e2016e1fbb898f05e03b2cbb2ad6
LM Train <layconmm@msn.com>
2020-02-12 21:33:46 -0600

    Get Portfolios from Client App

--- actions/index.js
+++ actions/index.js
@@ -14,11 +14,13 @@ const setAuthHeader = (req) => {
 }
 
 export const getSecretData = async (req) => {
-
     const url = 'http://localhost:3000/api/v1/secret';
    return await axios.get(url, setAuthHeader(req)).then(response =>  response.data);
 }
 
+export const getPortfolios = async(req) => {
+    return await axios.get('http://localhost:3000/api/v1/portfolios', setAuthHeader(req)).then(response => response.data);
+}
 // export const getSecretDataServer = async (req) => {
 //     return await axios.get('http://localhost:3000//api/v1/secret', setAuthHeader(req)).then(response =>  response.data);
 // }
