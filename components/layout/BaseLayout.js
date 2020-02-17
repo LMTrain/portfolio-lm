0c714a5e8fe074102e5e9700b047df01ab9531de
LM Train <layconmm@msn.com>
2020-02-16 19:04:01 -0600

    Blog Listing

--- components/layout/BaseLayout.js
+++ components/layout/BaseLayout.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import Header from '../shared/Header'
+import Head from 'next/head';
 
 const BaseLayout = (props) => {
     const { className, children, isAuthenticated, user } = props;
     const headerType = props.headerType || 'default';
 
     return (
-        <div className="layout-container" >
-            {/* { headerType === 'index' && <Header className="port-nav-index" isAuthenticated={isAuthenticated} user={user}/>}
-            { headerType === 'default' && <Header className="port-nav-default" isAuthenticated={isAuthenticated} user={user}/>} */}
-            
-            <Header className={`port-nav-${headerType}`} isAuthenticated={isAuthenticated} user={user}/>
-            <main className={`cover ${className}`}>
-                <div className="wrapper">
-                    {children}
-                </div>
-            </main>
-        </div>
+        <>
+        <head>
+            <title>Laycon Muriziq</title>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"/>
+        </head>
+            <div className="layout-container" >               
+                <Header className={`port-nav-${headerType}`} isAuthenticated={isAuthenticated} user={user}/>
+                <main className={`cover ${className}`}>
+                    <div className="wrapper">
+                        {children}
+                    </div>
+                </main>
+            </div>
+        </>
     )
 }
 
