57cf0555a64defda9fa92867d3ce8e5200cc3f50
LM Train <layconmm@msn.com>
2020-02-18 23:40:16 -0600

    Hover Menu Debugging

--- components/layout/BaseLayout.js
+++ components/layout/BaseLayout.js
@@ -11,6 +11,7 @@ const BaseLayout = (props) => {
         <head>
             <title>Laycon Muriziq</title>
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossOrigin="anonymous"/>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
         </head>
             <div className="layout-container" >               
                 <Header className={`port-nav-${headerType}`} isAuthenticated={isAuthenticated} user={user}/>
--- components/slate-editor/HoverMenu.js
+++ components/slate-editor/HoverMenu.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { StyledMenu } from "./components";
-import { renderMarkButton } from './renders';
 
+import { StyledMenu } from './components';
+import { renderMarkButton, renderBlockButton } from './renders';
 
 class HoverMenu extends React.Component {
 
-    renderMarkButton(type, icon) {
-        const { editor } = this.props;
+  renderMarkButton(type, icon) {
+    const { editor } = this.props;
+    return renderMarkButton(type, icon, editor);
+  }
 
-        return renderMarkButton(type, icon, editor);
-    }
-  
-    render() {
-        const { className, innerRef, editor } = this.props
-        const root = window.document.getElementById('__next')
+  renderBlockButton(type, icon) {
+    const { editor } = this.props;
+    return renderBlockButton(type, icon, editor);
+  }
 
-        return ReactDOM.createPortal(
-            <StyledMenu className={className} innerRef={innerRef}>
-                {this.renderMarkButton('bold', 'format_bold')}
-                {this.renderMarkButton('italic', 'format_italic')}
-                {this.renderMarkButton('underlined', 'format_underlined')}
-                {this.renderMarkButton('code', 'code')}
-            </StyledMenu>,
-            root
-        )
-    }
+  render() {
+    const { className, innerRef, editor } = this.props
+    const root = window.document.getElementById('__next')
+
+    return ReactDOM.createPortal(
+      <StyledMenu className={className} innerRef={innerRef}>
+        {this.renderMarkButton('bold', 'format_bold')}
+        {this.renderMarkButton('italic', 'format_italic')}
+        {this.renderMarkButton('underlined', 'format_underlined')}
+        {this.renderMarkButton('code', 'code')}
+        {this.renderBlockButton('heading-one', 'looks_one')}
+        {this.renderBlockButton('heading-two', 'looks_two')}
+        {this.renderBlockButton('block-quote', 'format_quote')}
+        {this.renderBlockButton('numbered-list', 'format_list_numbered')}
+        {this.renderBlockButton('bulleted-list', 'format_list_bulleted')}
+      </StyledMenu>,
+      root
+    )
+  }
 }
 
 export default HoverMenu;
