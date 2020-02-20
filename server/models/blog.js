a27e29088299f2d9d0ccb9159bb6b4203dd680e5
LM Train <layconmm@msn.com>
2020-02-19 20:41:45 -0600

    Create Blog Model

---
+++ server/models/blog.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//FUNCTION TO SET STRING TYPES
+const setStringType = (maxLength) => ({ type: String, required: true, maxlength: maxLength })
+
+const blogSchema = new Schema({
+    userId: { type: String, required: true },
+    slug: { type: String, unique: true, sparse: true},
+    title: setStringType(256),
+    subTitle: setStringType(256),
+    story: setStringType(600),   
+    createdAt: { type: Date, default: Date.now},
+    updatedAt: { type: Date, default: Date.now},
+    status: { type: String, required: true},
+    author: setStringType(256),
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
