ac98eb8341b26114b9714a1f748151392190f2df
LM Train <layconmm@msn.com>
2020-02-24 17:08:53 -0600

    Save Blog - Lock the request II

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -30,25 +30,32 @@ exports.getBlogById = (req, res) => {
 
 //ENDPOINT - POST DATA TO MONGODB
 exports.createBlog = (req, res) => {
-    lock.acquire(key, function(done) {       
-        const blogData = req.body;
-        const blog = new Blog(blogData);
-        if (req.user) {
-            blog.userId = req.user.sub;
-            blog.author = req.user.name;
-        }
-        console.log(blogData);
-        blog.save((err, createdBlog) => {
-            setTimeout(() => done(), 5000);
-            if (err) {
-                return res.status(422).send(err);
+    const lockId = req.query.lockId;
+
+    if (!lock.isBusy(lockId)) {
+
+        lock.acquire(lockId, function(done) {       
+            const blogData = req.body;
+            const blog = new Blog(blogData);
+            if (req.user) {
+                blog.userId = req.user.sub;
+                blog.author = req.user.name;
             }
-            return res.json(createdBlog);
-        }); 
-    }, function(err, ret) {
-        // lock released       
-        err && console.error(err)
-    });    
+            console.log(blogData);
+            blog.save((err, createdBlog) => {
+                setTimeout(() => done(), 5000);
+                if (err) {
+                    return res.status(422).send(err);
+                }
+                return res.json(createdBlog);
+            }); 
+        }, function(err, ret) {
+            // lock released       
+            err && console.error(err)
+        });    
+    } else {
+        return res.status(422).send({message: 'Blog is Saving...'});
+    }
     
 }
 
