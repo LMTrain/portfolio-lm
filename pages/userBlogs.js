2a621c8047d14b57be6c6de04fe0f4ea995040f5
LM Train <layconmm@msn.com>
2020-02-26 22:47:23 -0600

    Display User Blogs

--- pages/userBlogs.js
+++ pages/userBlogs.js
@@ -4,12 +4,14 @@ import BasePage from '../components/BasePage';
 import { Container, Row, Col } from 'reactstrap';
 
 import withAuth from '../components/hoc/withAuth';
+import { Link } from '../routes';
+
 import { getUserBlogs } from '../actions';
 
 class UserBlogs extends React.Component {
 
     static async getInitialProps({req}) {
-        let blogs = {};
+        let blogs = [];
 
         try {
             blogs = await getUserBlogs(req);
@@ -19,9 +21,39 @@ class UserBlogs extends React.Component {
         return {blogs};
     }
 
+    separateBlogs(blogs) {
+        const published = [];
+        const drafts = [];
+        
+        blogs.forEach((blog) => {
+            blog.status === 'draft' ? drafts.push(blog) : published.push(blog);
+        });
+
+        return {published, drafts};
+    }
+
+    renderBlogs(blogs) {
+        return (
+            <ul className="user-blogs-list">
+                {
+                    blogs.map((blog, index) => (
+                        <li key={index}>
+                            <Link route={`/blogs/${blog._id}/edit`}>
+                                <a>{blog.title}</a>
+                            </Link>
+                        </li>
+                        
+                        )
+                    )                    
+                }
+            </ul>
+        )
+    }
+
     render () {
         const {blogs} = this.props;
         console.log(blogs)
+        const {published, drafts} = this.separateBlogs(blogs);
         return (
             <BaseLayout {...this.props.auth} headerType={'landing'} >
             <div className="masthead" style={{"backgroundImage": "url('/static/images/home-bg.jpg')"}}>
@@ -40,10 +72,12 @@ class UserBlogs extends React.Component {
             <BasePage className="blog-user-page">
                 <Row>
                     <Col md="6" className="mx-auto text-center">
-                        Published Blogs
+                        <h2 className="blog-status-title">Published Blogs</h2>
+                        {this.renderBlogs(published)}
                     </Col>
                     <Col md="6" className="mx-auto text-center">
-                        Draft Blogs
+                        <h2 className="blog-status-title">Draft Blogs</h2>
+                        {this.renderBlogs(drafts)}
                     </Col>
                
                 </Row>
